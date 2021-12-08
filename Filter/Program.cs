c54646652e34b89b7e7b8ba176a1b68142e14fc4
Isaac Osment <isaac@bigtyre.com.au>
2021-12-08 12:40:49 +1000

    Add project files.

---
+++ Filter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Filter
+{
+    class Program
+    {
+        private static CancellationToken CancelToken;
+        private static ConcurrentQueue<string> InputLines { get; } = new();
+
+        static void Main(string[] args)
+        {
+            using var cancellationTokenSource = new CancellationTokenSource();
+            CancelToken = cancellationTokenSource.Token;
+
+
+            string filter = null;
+            if (args.Length > 0) { 
+                filter = string.Join(' ', args);
+            }
+            else
+            {
+                Console.Write("Enter filter: ");
+                filter = Console.ReadLine();
+            }
+
+            _ = Task.Run(() => ReadInput());
+
+            while (!CancelToken.IsCancellationRequested)
+            {
+                while (InputLines.TryDequeue(out var line))
+                {
+                    if (Regex.IsMatch(line, filter))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            Console.WriteLine("Exited");
+        }
+
+        private static void ReadInput()
+        {
+            while (!CancelToken.IsCancellationRequested) {
+                var line = Console.ReadLine();
+                if (line == null) continue;
+
+                InputLines.Enqueue(line);
+            }
+        }
+    }
+}
---
+++ OutputGenerator/Program.cs
@@ -0,0 +1,8 @@
+﻿var random = new Random();
+
+while (true)
+{
+    int next = random.Next(0, 9);   
+    Console.WriteLine(next);
+    await Task.Delay(500);
+}
